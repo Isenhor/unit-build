246b5595f1891c2720d6d050f7c2238fc066016e
Isenhor <31591656+Isenhor@users.noreply.github.com>
2023-11-29 22:03:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,22 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".burger");
   const menu = document.querySelector("#menu");
   const bodyEl = document.body;
+  const navBtn = document.querySelector(".nav-btn");
 
   burger.addEventListener("click", function () {
     if (this.classList.contains("burger--active")) {
       this.classList.remove("burger--active");
+      navBtn.classList.remove("nav-btn--active")
       bodyEl.classList.remove("lock");
-      menu.classList.remove("header-nav--active");
+      menu.classList.remove("nav--active");
     } else {
       this.classList.add("burger--active");
+      navBtn.classList.add("nav-btn--active")
       bodyEl.classList.add("lock");
-      menu.classList.add("header-nav--active");
+      menu.classList.add("nav--active");
     }
   });
 
   menu.addEventListener("click", () => {
     burger.classList.remove("burger--active");
     bodyEl.classList.remove("lock");
-    menu.classList.remove("header-nav--active");
+    menu.classList.remove("nav--active");
   });
 });
